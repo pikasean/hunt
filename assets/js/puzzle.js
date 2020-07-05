654fab7ea3e59abd9214671a43fc869db9c7f5c3
Chong Jing Quan <jingquan0302@gmail.com>
2020-07-05 18:32:19 +0800

    Update puzzle.js

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -60,10 +60,10 @@ function showSubmit() {
           <div id="check_answer" class="header-link"><a href="${puzzle}.html#" data-toggle="modal" id="checkAnswerButton"
                                                     data-target="#checkAnswerModal">Submit Answer</a></div>
 <!--          <div id="submit" class="header-link"><a href="../solutions/${puzzle}.html">Solution</a></div>-->
-                <div class="header-link header-dropdown">          
+                <div class="header-link header-dropdown">
                     <div id="welcome" class="header-dropdown-title"><a>${welcomeDescription}<span class="fas fa-caret-down"></span></a></div>
                         <div class="dropdown-content-wrapper">
-                            <div class="dropdown-link"><a id="logout">Logout</a></div>
+                            <div class="dropdown-link">Logout</div>
                         </div>
                     </div>
                 </div>
