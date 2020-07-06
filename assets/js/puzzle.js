b5c951f86b9b28b0ad32f63aa3e37dbfe6ee3239
Chong Jing Quan <jingquan0302@gmail.com>
2020-07-06 20:32:50 +0800

    Update puzzle.js

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -221,6 +221,7 @@ function renderOnSubmitOrVoided(event) {
         .then((data) => {
             if(data.hint){
                 $('#hint').prop('disabled', true);
+                console.log(data.hint)
                 $('checkAnswerResult').removeClass('correct').removeClass('incorrect')
                     .text(`${data.hint}`)
             }
